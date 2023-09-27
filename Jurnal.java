98a29f7f6f64ebedeec6c4e03c003c4404553a92
irvanmaulanaaa <irvanm166@gmail.com>
2023-09-27 09:35:36 +0700

    commit pertama

---
+++ Jurnal.java
@@ -0,0 +1,7 @@
+public class Jurnal {
+    public static void main  (String [] args) {
+        System.out.println("NAMA: Irvan Maulana");
+        System.out.println("NIM: 607062330013");
+        System.out.println("KELAS: D3IF-47-04");
+    }
+}
